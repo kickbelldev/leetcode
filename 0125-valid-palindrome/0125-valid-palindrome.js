5e5d33bf494cee7c00644e66cf398cfc07fb1c2b
Jongchan <kickbelldev@gmail.com>
2023-02-02 16:42:02 +0900

    Time: 78 ms (73.32%), Space: 52.1 MB (6.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,12 +4,9 @@
  */
 var isPalindrome = function(s) {
     const arr = s.toLowerCase().split('').filter((v) => /[0-9a-z]/.test(v))
-    
-    while (arr.length) {
-        const left = arr.shift()
-        const right = arr.pop()
-        if (!left || !right) break
-        if (left !== right) {
+    const reversed = [...arr].reverse()
+    for (const i in arr) {
+        if (arr[i] !== reversed[i]) {
             return false
         }
     }
