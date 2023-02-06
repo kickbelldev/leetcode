265520f032629bc4419ddb4cf0dae25394799f7f
Jongchan <kickbelldev@gmail.com>
2023-02-06 20:52:28 +0900

    Time: 143 ms (47.46%), Space: 54.5 MB (19.47%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const obj = {}
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('')
+        
+        if (!obj[sorted]) {
+            obj[sorted] = []
+        }
+        
+        obj[sorted].push(str)
+    }
+    
+    return Object.values(obj)
+};
