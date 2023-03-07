44a6e577f9d2f2ad84d28afda232a1210765cf30
Jongchan <kickbelldev@gmail.com>
2023-03-07 17:21:24 +0900

    Time: 99 ms (89.40%), Space: 55.6 MB (33.04%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const out = []
+    let p = 1
+    for (let i = 0; i < nums.length; i++) {
+        out.push(p)
+        p *= nums[i]
+    }
+    
+    p = 1
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        out[i] *= p
+        p *= nums[i]
+    }
+    
+    return out
+};
