404dae08378b430b09ce843dbc4d86b91a65604e
Jongchan Kim <kickbelldev@gmail.com>
2025-03-13 09:06:50 +0900

    Time: 13 ms (56.02%), Space: 63.8 MB (15.28%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.ts
@@ -0,0 +1,44 @@
+function findOrder(numCourses: number, prerequisites: number[][]): number[] {
+  const courseGraph: {
+    degree: number
+    set: Set<number>
+  }[] = Array(numCourses).fill(undefined).map(() => ({
+    degree: 0,
+    set: new Set()
+  }))
+
+  for (const [a, b] of prerequisites) {
+    courseGraph[b].degree++
+    courseGraph[a].set.add(b)
+  }
+
+  const queue = []
+
+  for (let i = 0; i < numCourses; i++) {
+    if (courseGraph[i].degree === 0) {
+      queue.push(i)
+    }
+  }
+
+  const result: number[] = []
+
+  while (queue.length) {
+    const current = queue.shift()
+    result.push(current)
+
+    for (const next of courseGraph[current].set) {
+      courseGraph[next].degree -= 1
+      if (courseGraph[next].degree === 0) {
+        queue.push(next)
+      }
+    }
+  }
+
+  console.log(result)
+
+  if (result.length !== numCourses) {
+    return []
+  }
+
+  return result.reverse()
+}
